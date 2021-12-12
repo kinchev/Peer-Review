304baab1a1d41c07a6ce8e50e786b5d1dd6e6c1e
Alexander Dimov <@Alexdim71>
2021-12-13 00:00:07 +0200

    Home page slight improvements

--- src/main/java/com/telerik/peer/controllers/mvc/HomeMvcController.java
+++ src/main/java/com/telerik/peer/controllers/mvc/HomeMvcController.java
@@ -3,7 +3,10 @@ package com.telerik.peer.controllers.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/")
@@ -14,12 +17,10 @@ public class HomeMvcController {
 //        this.userService = userService;
 //    }
 
-
-    //
-//    @ModelAttribute("isAuthenticated")
-//    public boolean populateIsAuthenticated(HttpSession session) {
-//        return session.getAttribute("currentUser") != null;
-//    }
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
     @GetMapping
     public String homePage(Model model) {
         return "index";
